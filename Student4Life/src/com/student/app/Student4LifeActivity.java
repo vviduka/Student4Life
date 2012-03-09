9fae963c2709788e7d376a6676d73b596ba9ea74
dblagajac1 <danijel.blagajac@gmail.com>
2012-03-09 18:49:37 +0100

    Napravio sam neku test app s buttonima

--- Student4Life/src/com/student/app/Student4LifeActivity.java
+++ Student4Life/src/com/student/app/Student4LifeActivity.java
@@ -1,13 +1,41 @@
 package com.student.app;
 
 import android.app.Activity;
+
 import android.os.Bundle;
 
-public class Student4LifeActivity extends Activity {
+import android.view.View;
+import android.widget.EditText;
+
+
+
+
+
+public class Student4LifeActivity extends Activity 
+{
+	private EditText text;
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        text = (EditText) findViewById(R.id.editText1);
+        text.setText("Nije pritisnut button");
     }
+       
+    //button1
+    public void MojButton1(View view) {
+
+    text.setText("Button 1 was clicked");
+    }
+    
+    //button2
+    
+    public void MojButton2 (View view) {
+    	
+    	text.setText("Button 2 was clicked");
+    	}
+    
 }
+        
+
