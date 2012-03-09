3ed5b644032b6f7d21f2baacb90baa2025f9cb4a
dblagajac1 <danijel.blagajac@gmail.com>
2012-03-09 10:46:22 +0100

    Prvi push

---
+++ Student4Life/src/com/student/app/Student4LifeActivity.java
@@ -0,0 +1,13 @@
+package com.student.app;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Student4LifeActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
