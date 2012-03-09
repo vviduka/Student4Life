eca2cce0c00e9499e36de3dabdbc4a9980e8701c
dblagajac1 <danijel.blagajac@gmail.com>
2012-03-09 12:33:17 +0100

    source code test

--- Student4Life/src/com/student/app/klasa.java
+++ Student4Life/src/com/student/app/klasa.java
@@ -1,6 +1,46 @@
 package com.student.app;
 
-public class klasa {
+import android.app.Activity;
+import android.view.View;
+import android.view.Window;
+
+public class klasa extends Activity{
+
+	/* (non-Javadoc)
+	 * @see android.app.Activity#getWindow()
+	 */
+	@Override
+	public Window getWindow() {
+		// TODO Auto-generated method stub
+		return super.getWindow();
+	}
+
+	@Override
+	public boolean equals(Object o) {
+		// TODO Auto-generated method stub
+		return super.equals(o);
+	}
+
+	@Override
+	public void closeOptionsMenu() {
+		// TODO Auto-generated method stub
+		super.closeOptionsMenu();
+	}
+
+	@Override
+	public View findViewById(int id) {
+		// TODO Auto-generated method stub
+		return super.findViewById(id);
+	}
+
+	public klasa() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
 
 }
 //nova klasa
+
+
+
+
