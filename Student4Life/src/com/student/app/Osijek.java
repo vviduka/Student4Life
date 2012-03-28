844bdb4602376d968420cd972d66a32b495dbd59
Vjekoslav Viduka <vviduka@gmail.com>
2012-03-28 22:07:18 +0200

    dodana nova klasa osijek i dodan u manifest permission i google api

---
+++ Student4Life/src/com/student/app/Osijek.java
@@ -0,0 +1,24 @@
+package com.student.app;
+
+
+import com.google.android.maps.MapActivity;
+import com.google.android.maps.MapView;
+import android.os.Bundle;
+
+public class Osijek extends MapActivity {
+	MapView map;
+	@Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+        map =(MapView)findViewById(R.id.mvOsijek);
+        map.setBuiltInZoomControls(true);
+    }
+
+	@Override
+	protected boolean isRouteDisplayed() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+	
+}
