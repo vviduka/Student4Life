03173149a9db2732a61b06bb9411e14675c2bfe2
Vjekoslav Viduka <vviduka@gmail.com>
2012-04-01 19:16:27 +0200

    potavio focus mape na osijek

--- Student4Life/src/com/student/app/Osijek.java
+++ Student4Life/src/com/student/app/Osijek.java
@@ -1,18 +1,25 @@
 package com.student.app;
 
 
+import com.google.android.maps.GeoPoint;
 import com.google.android.maps.MapActivity;
+import com.google.android.maps.MapController;
 import com.google.android.maps.MapView;
 import android.os.Bundle;
 
 public class Osijek extends MapActivity {
 	MapView map;
+	MapController controller;
 	@Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.osijek);
         map =(MapView)findViewById(R.id.mvOsijek);
         map.setBuiltInZoomControls(true);
+        controller = map.getController();
+        GeoPoint point = new GeoPoint((int)(45.5599888 * 1E6), (int)(18.6758628 * 1E6));
+        controller.animateTo(point);
+        controller.setZoom(13);
     }
 
 	@Override
